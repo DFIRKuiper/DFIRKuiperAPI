b061322feb5a9b2da4517702b9e0b1692f9aedf5
saleh muhaysin <salehmuhaysin@gmail.com>
2021-11-05 16:20:53 +0300

    support multiple indices

--- GetFieldsScript.py
+++ GetFieldsScript.py
@@ -3,9 +3,10 @@
 from KuiperAPI import Kuiper_API
 import argparse
 
+import json 
 
-Kuiper_URL = "https://192.168.1.110/api/"
-api_token = ""
+Kuiper_URL = "https://192.168.220.5/api/"
+api_token = "API_TOKEN"
 
 
 def getJsonByPath(json , path):
@@ -26,10 +27,11 @@ def main():
 	requiredargs.add_argument('-f' , dest='fields' ,              help='specific fields to retrieve (split by comma)', required=True)
 	requiredargs.add_argument('-o' , dest='output_file' ,         help='Output file to store the results to', required=True)
 
-	parser.add_argument('-u' , dest='url' ,                 help='API URL for kuiper (default: '+Kuiper_URL+')' , default=Kuiper_URL)
-	parser.add_argument('-t' , dest='token' ,               help='API token (default:<taken from script>)' , default=api_token)
-	parser.add_argument('-q' , dest='query' ,               help='query string for elasticsearch', default='*')
-	parser.add_argument('-s' , dest='sort_by' ,             help='field to sort by', default=None)
+	parser.add_argument('-u'  , dest='url' ,                 help='API URL for kuiper (default: '+Kuiper_URL+')' , default=Kuiper_URL)
+	parser.add_argument('-t'  , dest='token' ,               help='API token (default:<taken from script>)' , default=api_token)
+	parser.add_argument('-q'  , dest='query' ,               help='query string for elasticsearch', default='*')
+	parser.add_argument('-s'  , dest='sort_by' ,             help='field to sort by', default=None)
+	parser.add_argument('-cs' , dest='chunk_size' ,          help='field to sort by', default=30)
 
 
 	args = parser.parse_args()
@@ -38,24 +40,39 @@ def main():
 	# ================= get records from kuiper
 	k 		= Kuiper_API(args.url , args.token)
 	sort_by = None if args.sort_by is None else {'name':args.sort_by , 'order':0 }
-	res 	= k.get_artifacts(case=args.case_id , query_string=args.query,sort_by=sort_by , fields=args.fields)
+	fields  = None if args.fields is None or args.fields == "*" else args.fields
+	res 	= k.get_artifacts(case=args.case_id , query_string=args.query,sort_by=sort_by , fields=fields , chunk_size=int(args.chunk_size))
 
+	isJson = True if fields is None else False # if there are no selected fields, write output to json file
 	output = open(args.output_file, 'w')
-	fields = args.fields.split(",")
-	output.write(args.fields + "\n")
+
+	if not isJson:
+		output.write(fields + "\n")
+		fields_list = fields.split(",")
 
 	for r in res:
-		hashes = []
+		if 'success' in r.keys() and r['success'] == False:
+			print("Failed to fetch the records: %s" % (r['message']))
+			break
 		for record in r['data']['hits']['hits']:
-			rec = []
-			for f in fields:
-				rec.append(getJsonByPath(record['_source'] , f))
-			line = ','.join(rec)
-			if line.replace("," , "") != "": # if the line is not empty
-				output.write(line  + "\n")
+			# if output is json, then write all the records
+			if isJson:
+				output.write(json.dumps({"_source": record['_source'] , "_index" : record['_index']}) + "\n")
+
+			# if output is not json, then write only specified records
+			else:
+				rec = []
+				for f in fields_list:
+					if f == "index" or f == "case_id": 
+						rec.append(record['_index'])
+					else: 
+						rec.append(getJsonByPath(record['_source'] , f))
+
+				line = ','.join(rec)
+				if line.replace("," , "") != "": # if the line is not empty
+					output.write(line  + "\n")
 			
 		print("total:%d, retrieved_records:%d, chunk_number: %d" % (r['total'] , r['retrieved_records'] , r['seq_number']))
-
 	output.close()
 
 
--- KuiperAPI.py
+++ KuiperAPI.py
@@ -18,7 +18,7 @@ class Kuiper_API:
 	# sort_by: 		the field to sort by (example: {'name': 'Data.@timestamp' , 'order': 0}) >> 0=asc, 1=desc
 	# fields: 		retrive specific fields from the server (if not specified it will return all the values), split fields by comma
 	# return: this function return a generator that retrieves the data from the server as a chunks 
-	def get_artifacts(self, case , query_string , sort_by=None, fields=None):
+	def get_artifacts(self, case , query_string , sort_by=None, fields=None, chunk_size=30):
 
 		total_records 		= -1 # this store the number of records in the database
 		retrieved_records 	= 0  # this is the number of records retrieved so far 
@@ -28,26 +28,30 @@ class Kuiper_API:
 			'query' 	: query_string,
 			'sort_by' 	: sort_by,
 			'fields' 	: fields,
-			'api_token' :self.api_token 
+			'api_token' : self.api_token,
+			'case_id'	: case,
+			'chunk_size': chunk_size
 		}
 		
 		while retrieved_records < total_records or seq_number == 0:
 
 			json_request['seq_num'] = seq_number
 			json_string = json.dumps({'data': json_request})
-
-			response = requests.post(self.Kuiper_URL + "get_artifacts/"+case, data=json_string , verify=False)
-
+			response = requests.post(self.Kuiper_URL + "get_artifacts", data=json_string , verify=False)
 			response_json 	 = json.loads(response.content)
-			retrieved_records += len(response_json['data']['hits']['hits'])
-			total_records 	 = response_json['total']
+			if response_json['success']:
+				retrieved_records += len(response_json['data']['hits']['hits'])
+				total_records 	 = response_json['total']
 
-			response_json['retrieved_records'] = retrieved_records
-			response_json['seq_number'] 	   = seq_number
+				response_json['retrieved_records'] = retrieved_records
+				response_json['seq_number'] 	   = seq_number
 
-			seq_number		 +=1 
+				seq_number		 +=1 
+				yield response_json
+			else:
+				yield response_json
 
-			yield response_json
+
 
 
 
